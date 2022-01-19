04dca5a52241977d42cd6f8bcb6611c5a3bc7e2b
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-01-18 23:11:02 -0300

    @BruRaptor[CRUD simples com Spring]

---
+++ Aula Generation Spring 2/CrudSpring/src/main/java/com/Atividade/crud/CrudSpring/controller/PostagemController.java
@@ -0,0 +1,28 @@
+package com.Atividade.crud.CrudSpring.controller;
+
+import java.util.List;
+
+import com.Atividade.crud.CrudSpring.model.Postagem;
+import com.Atividade.crud.CrudSpring.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+}
